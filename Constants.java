1e6a45c101bfeffb9125ddc85fa91c92daf5166c
gogiakush <49599534+gogiakush@users.noreply.github.com>
2020-05-06 19:18:20 -0700

    Create Constants.java

---
+++ Constants.java
@@ -0,0 +1,20 @@
+import java.awt.Color;
+import java.awt.Font;
+
+public class Constants {
+	
+	public static final Font font = new Font("SansSerif", Font.PLAIN, Photoshop.WIDTH/60);
+	public static final Color[] colors = {
+			Color.red,
+			Color.orange, 
+			Color.yellow, 
+			Color.green,
+			Color.blue,
+			Color.pink,
+			Color.black
+	};
+	
+	public static final String projectDirectory = "myPhotoshop/";
+	public static final String projectDirectorySerialized = "myPhotoshopObject/";
+	
+}
